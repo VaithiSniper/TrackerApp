3481eefcf457ed15c54ce000b95fa4f9e3afc0fa
VaithiSniper <vaithi.genghiskhan@gmail.com>
2022-05-13 01:54:12 +0530

    auth issue

--- appwrite.config.js
+++ appwrite.config.js
@@ -1,6 +1,6 @@
 import { Appwrite } from "appwrite";
 
-const api_end = "https://150.230.140.97/v1";
+const api_end = "http://150.230.140.97/v1";
 const id = "626fab2b4a82dbb03d88";
 
 const appwrite = new Appwrite();
